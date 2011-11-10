d4e4c3871a411aea0ac2babec879ce8f1a232c3d
Alard <alard@example.com>
2011-11-11 00:18:25 +0100

    Initial commit.

---
+++ dld-profile.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+# Script for downloading the contents of one splinder.com profile.
+# (Not including the blog.)
+#
+# Usage:   dld-profile.sh ${USERNAME}
+#
+
+VERSION="20111110.01"
+
+# this script needs wget-warc, which you can find on the ArchiveTeam wiki.
+
+WGET_WARC=./wget-warc
+if [ ! -x $WGET_WARC ]
+then
+  echo "./wget-warc not found."
+  exit 3
+fi
+
+# the script also needs curl
+
+if ! builtin type -p curl &>/dev/null
+then
+  echo "You don't have curl."
+  exit 3
+fi
+
+USER_AGENT="Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+
+username="$1"
+userdir="data/${username:0:1}/${username:0:2}/${username:0:3}/${username}/profile"
+
+if [[ -f "${userdir}/.incomplete" ]]
+then
+  echo "  Deleting incomplete result for ${username}"
+  rm -rf "${userdir}"
+fi
+
+mkdir -p "${userdir}"
+touch "${userdir}/.incomplete"
+
+echo "  Downloading ${username} profile"
+
+echo -n "   - Downloading profile HTML pages..."
+$WGET_WARC -U "${USER_AGENT}" -e "robots=off" \
+    -nv -o "$userdir/wget-phase-1.log" \
+    --directory-prefix="$userdir/files/" \
+    --warc-file="$userdir/splinder.com-${username}-html" \
+    --warc-max-size=inf \
+    --warc-header="operator: Archive Team" \
+    --warc-header="splinder-dld-script-version: ${VERSION}" \
+    --warc-header="splinder-username: ${username}" \
+    -r -l inf --no-remove-listing \
+    --no-timestamping \
+    --trust-server-names \
+    --adjust-extension \
+    -I "/profile/${username}/friends" \
+    -I "/profile/${username}/friendof" \
+    -I "/profile/${username}/blogs" \
+    -I "/mediablog/${username}" \
+    -I "/media/comment/list/" \
+    -R "sizes" \
+    "http://www.splinder.com/profile/${username}/" \
+    "http://www.splinder.com/ajax.php?type=counter&op=profile&profile=${username}" \
+    "http://www.splinder.com/profile/${username}/friends/" \
+    "http://www.splinder.com/profile/${username}/friendof/" \
+    "http://www.splinder.com/profile/${username}/blogs/" \
+    "http://www.splinder.com/mediablog/${username}/"
+result=$?
+if [ $result -ne 0 ] && [ $result -ne 6 ] && [ $result -ne 8 ]
+then
+  echo " ERROR ($result)."
+  exit 1
+fi
+echo " done."
+
+echo -n "   - Parsing profile HTML to extract media urls..."
+find "$userdir/files/" -name "*.html" \
+  | python extract-urls-from-html.py \
+  > "$userdir/media-urls.txt"
+echo " done."
+
+num_urls=$(cat "$userdir/media-urls.txt" | wc -l)
+
+echo -n "   - Downloading ${num_urls} media files..."
+if [[ $num_urls -ne 0 ]]
+then
+  $WGET_WARC -U "${USER_AGENT}" -e "robots=off" \
+      -nv -o "$userdir/wget-phase-2.log" \
+      -O /dev/null \
+      --warc-file="$userdir/splinder.com-${username}-media" \
+      --warc-max-size=inf \
+      --warc-header="operator: Archive Team" \
+      --warc-header="splinder-dld-script-version: ${VERSION}" \
+      --warc-header="splinder-username: ${username}" \
+      -i "$userdir/media-urls.txt"
+  result=$?
+  if [ $result -ne 0 ] && [ $result -ne 6 ] && [ $result -ne 8 ]
+  then
+    echo " ERROR ($result)."
+    exit 1
+  fi
+fi
+echo " done."
+
+blog_domains=$(
+  grep -oE '<a href="http://[^." ]+\.splinder.com' \
+       "${userdir}/files/www.splinder.com/profile/${username}/blogs/index.html" \
+    | cut -c 17- \
+    | grep -vE "(edit|journal|manuale|www).splinder.com"
+)
+for blog_domain in $blog_domains
+do
+  echo -n "   - Downloading blog from ${blog_domain}..."
+  $WGET_WARC -U "${USER_AGENT}" -e "robots=off" \
+      -nv -o "$userdir/wget-phase-3-${blog_domain}.log" \
+      --directory-prefix="$userdir/files/" \
+      --warc-file="$userdir/splinder.com-${username}-blog-${blog_domain}" \
+      --warc-max-size=inf \
+      --warc-header="operator: Archive Team" \
+      --warc-header="splinder-dld-script-version: ${VERSION}" \
+      --warc-header="splinder-username: ${username}" \
+      -r -l inf --no-remove-listing \
+      --no-timestamping \
+      --page-requisites --trust-server-names \
+      --span-hosts \
+      --domains="${blog_domain},files.splinder.com,www.splinder.com,syndication.splinder.com" \
+      --exclude-directories="/users,/media,/node,/profile,/mediablog,/community,/user,/night,/home,/mysearch,/online,/trackback,/myblog/post,/myblog/posts,/myblog/tags,/myblog/tag,/myblog/view,/myblog/latest,/myblog/subscribe,/myblog/comment/reply,/myblog/comments/latest,/post,/posts" \
+      "http://${blog_domain}/"
+  result=$?
+  if [ $result -ne 0 ] && [ $result -ne 6 ] && [ $result -ne 8 ]
+  then
+    echo " ERROR ($result)."
+    exit 1
+  fi
+  echo " done."
+done
+
+echo -n "   - Result: "
+if du --help | grep -q apparent-size
+then
+  du --apparent-size -hs "$userdir/"* | cut -f 1
+else
+  du -hs "$userdir/"* | cut -f 1
+fi
+
+rm "${userdir}/.incomplete"
+
+exit 0
+
+
+
---
+++ extract-urls-from-html.py
@@ -0,0 +1,72 @@
+from BeautifulSoup import BeautifulSoup
+import fileinput
+import re
+
+class UrlCollector:
+  FILES_URL_RE = re.compile("http://files.splinder.com/[a-z0-9_\.]+")
+  FILE_JPG_URL_RE = re.compile("^(http://files.splinder.com/[a-z0-9]+)[_a-z]*(\.(jpg|gif|png))$")
+  FILE_MP3_URL_RE = re.compile("^(http://files.splinder.com/[a-z0-9]+)[_a-z]*(\.mp3)$")
+  FILE_FLV_URL_RE = re.compile("^(http://files.splinder.com/[a-z0-9]+)[_a-z]*(\.flv)$")
+
+  def __init__(self):
+    self.urls = set()
+
+  def process(self, soup):
+    for singolo in soup.findAll('div', 'singolo-media'):
+      for img in singolo.findAll('img', attrs={'src' : self.FILES_URL_RE}):
+        self.addUrlsFromString(img['src'])
+
+      for a in singolo.findAll('a', attrs={'href' : self.FILES_URL_RE}):
+        self.addUrlsFromString(a['href'])
+
+      for param in singolo.findAll('param', attrs={'name' : ('flashvars','movie')}):
+        self.addUrlsFromString(param['value'])
+
+    for mediaFooter in soup.findAll('div', 'media-footer'):
+      for img in mediaFooter.findAll('img', attrs={'src' : self.FILES_URL_RE}):
+        self.addUrlsFromString(img['src'])
+
+    for profilo in soup.findAll('div', 'profilo'):
+      for img in profilo.findAll('img', attrs={'src' : self.FILES_URL_RE}):
+        self.addUrlsFromString(img['src'])
+
+  def addUrlsFromString(self, string):
+    for url in re.findall(self.FILES_URL_RE, string):
+      self.addUrl(url)
+
+  def addUrl(self, url):
+    self.urls.add(url)
+
+    m = re.match(self.FILE_JPG_URL_RE, url)
+    if m:
+      for size in ('_square','_medium','_thumbnail',''):
+        self.urls.add(''.join((m.group(1), size, m.group(2))))
+
+    m = re.match(self.FILE_MP3_URL_RE, url)
+    if m:
+      for size in ('_thumbnail',''):
+        self.urls.add(''.join((m.group(1), size, m.group(2))))
+
+    m = re.match(self.FILE_FLV_URL_RE, url)
+    if m:
+      self.urls.add(''.join((m.group(1), '.flv')))
+      for size in ('_square','_small','_thumbnail',''):
+        self.urls.add(''.join((m.group(1), size, '.jpg')))
+
+  def __iter__(self):
+    return self.urls.__iter__()
+
+
+urls = UrlCollector()
+
+for line in fileinput.input():
+  f = open(line.strip())
+  html = f.read()
+  f.close()
+
+  soup = BeautifulSoup(html)
+  urls.process(soup)
+
+for url in urls:
+  print url
+
