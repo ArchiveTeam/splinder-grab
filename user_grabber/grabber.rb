10e96e654a348952ee51eed96e00c60273e6bfeb
David Yip <yipdw@member.fsf.org>
2011-11-11 01:15:18 -0600

    A tool to scrape usernames from www.splinder.com.

---
+++ user_grabber/grabber.rb
@@ -0,0 +1,55 @@
+require 'connection_pool'
+require 'girl_friday'
+require 'mechanize'
+require 'nokogiri'
+require 'redis'
+
+BASE_URL = "http://www.splinder.com/users/list"
+CATEGORIES = ['a'..'z', '0'..'9', ['_']].map(&:to_a).inject(&:+)
+CONCURRENCY_LEVEL = 6
+
+# ---
+
+def goes_to_next_page?(link)
+  link.inner_html =~ /successiva/
+end
+
+redis_connections = ConnectionPool.new(:size => CONCURRENCY_LEVEL) { Redis.new }
+
+page_workers = GirlFriday::WorkQueue.new(:page_workers, :size => CONCURRENCY_LEVEL) do |agent, page|
+  puts "Working on #{page.uri}..."
+
+  redis_connections.with_connection do |redis|
+    doc = Nokogiri.HTML(page.body)
+
+    puts "Parsing #{page.uri} for user links..."
+    (doc/'.main_content .user-data a').map { |link| link.attribute('href').value }.each do |href|
+      if href =~ %r{/profile/(.+)}
+        redis.sadd('users', $1)
+      end
+    end
+
+    next_links = (doc/'#pager a')
+    go_to_next_page = next_links.detect { |l| goes_to_next_page?(l) }
+
+    if go_to_next_page
+      next_page = agent.click(go_to_next_page)
+
+      page_workers << [agent, next_page]
+    end
+
+    count = redis.scard('users')
+
+    puts "Found #{count} usernames."
+  end
+end
+
+CATEGORIES.each do |category|
+  agent = Mechanize.new
+  agent.user_agent_alias = 'Linux Firefox'
+  initial_page = agent.get("#{BASE_URL}/#{category}")
+
+  page_workers << [agent, initial_page]
+end
+
+page_workers.wait_for_empty
