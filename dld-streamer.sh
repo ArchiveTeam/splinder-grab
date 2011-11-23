d3e65bbe0c9520199a06c0eeb6d47e059c7d9876
Thad Ward <coderjoe69@yahoo.com>
2011-11-23 17:19:36 -0500

    add optional list file parameter
    add an optional parameter: username list filename
    If specified, read usernames from the list rather than getting them from
    the tracker. This allows retrying errored usernames stored in the
    error-usernames file output by this script.

--- dld-streamer.sh
+++ dld-streamer.sh
@@ -9,7 +9,11 @@
 # copy the wget executable as wget-warc to the same directory
 # as these scripts.
 #
-# Usage:   dld-streamer.sh ${YOURALIAS} ${MAX_THREADS}
+# Usage:   dld-streamer.sh ${YOURALIAS} ${MAX_THREADS} [usernamefile]
+#
+# If usernamefile is provided, it will get a list of usernames from
+# the named file, rather than the tracker. This is useful for things
+# like retrying users that errored before.
 #
 # To stop the script gracefully,  touch STOP  in the script's
 # working directory. The script will then finish the current
@@ -46,12 +50,20 @@ fi
 
 youralias="$1"
 max_jobs="$2"
+itemfile="$3"
 
 if [[ ! $youralias =~ ^[-A-Za-z0-9_]+$ ]]
 then
   echo "Usage:  $0 {nickname} {count}"
   echo "Run with a nickname with only A-Z, a-z, 0-9, - and _"
   echo "Count is maximum number of child processes to run."
+  exit 4
+fi
+
+if [[ -n "$itemfile" && ! -r "$itemfile" ]]
+then
+  echo "item list file \"$itemfile\" does not exist or is"
+  echo "not readable."
   exit 4
 fi
 
@@ -78,6 +90,12 @@ then
   initial_stop_mtime=$( stat -c '%Y' STOP )
 fi
 
+# open the file, if specified
+if [[ -n "$itemfile" ]]
+then
+  exec 4< "$itemfile"
+fi
+
 declare -A pids_users
 
 fork_more=1
@@ -100,28 +118,46 @@ do
 
   if [[ $( jobs -p -r | wc -l ) -lt $max_jobs && $fork_more -eq 1 ]]
   then
-    # request a username
-    outputstartline "$count Getting next username from tracker..."
-    tracker_no=$(( RANDOM % 3 ))
-    tracker_host="splinder-${tracker_no}.heroku.com"
-    username=$( curl -s -f -d "{\"downloader\":\"${youralias}\"}" http://${tracker_host}/request )
-
-    # empty?
-    if [ -z $username ]
+    if [[ -z "$itemfile" ]]
     then
-      echo
-      outputline "Tracker uncooperative.  Pausing for 30 seconds..."
-      echo
-      sleep 30
+      # request a username
+      outputstartline "$count Getting next username from tracker..."
+      tracker_no=$(( RANDOM % 3 ))
+      tracker_host="splinder-${tracker_no}.heroku.com"
+      username=$( curl -s -f -d "{\"downloader\":\"${youralias}\"}" http://${tracker_host}/request )
+
+      # empty?
+      if [ -z $username ]
+      then
+        echo
+        outputline "Tracker uncooperative.  Pausing for 30 seconds..."
+        echo
+        sleep 30
+      fi
     else
+      outputstartline "$count Reading next username from list..."
+      # read until we get a non-empty line or end-of-file
+      username=
+      while [[ -z "$username" && $fork_more -eq 1 ]]
+      do
+        read -u 4 username
+        if [[ $? -ne 0 ]]
+        then
+          outputendline " end of list reached."
+          fork_more=0
+        fi
+     done
+    fi
+
+    # not empty?
+    if [ -n $username ]
+    then
       outputendline " downloading ${username}"
       echo $username >> downloads.log
 
       ./dld-single.sh "$youralias" "$username" > "./logs/${username}.log" &
       pids_users["$!"]="$username"
     fi
-#  else
-#    echo "$count not requesting (fork_more = $fork_more )"
   fi
 
   # reap dead children
